470dba8a7835ddd162384d40593e75f00b3d1a2b
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-04 15:29:24 +0900

    画像がデータベースに入っていないときに、画像が表示されないように変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,18 +2,20 @@
 
 @section('content')
     <div class="container">
-        <a href="#" class="btn btn-outline-primary me-2 mb-3">投稿</a>
+        <a href="{{ route('post.create') }}" class="btn btn-outline-primary me-2 mb-3">投稿</a>
 
         @foreach ($posts as $post)
             <div class="border">
                 <div class="m-4">
                     <h5>投稿者: {{ $post->nickname }}</h5>
                     <h5>施設の名前: {{ $post->sauna_name }}</h5>
-                    <div>{{ $post->image_path }}</div>
-                    <p>{{ $post->content }}</p>
+                    <p>口コミ: <br>{{ $post->content }}</p>
+                    @if ($post->image_path)
+                    <div class="m-2"><img src="upload/{{ $post->image_path }}" width="300"></div>
+                    @endif
     
                     <div>
-                        <a href="#" class="btn btn-outline-primary me-2">詳細</a>
+                        <a href="#" class="btn btn-outline-primary me-2">コメントをする</a>
                         <a href="#" class="btn btn-outline-primary me-2">編集</a>
                         <a href="#" class="btn btn-outline-danger">削除</a>
                     </div>
