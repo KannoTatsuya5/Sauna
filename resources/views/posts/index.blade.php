a790ecdbabc40e687a9200ffb78137650d55914d
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-04 13:31:58 +0900

    投稿ボタンの作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,14 +2,24 @@
 
 @section('content')
     <div class="container">
-        @foreach ($posts as $post)
-            <div>{{ $post->nickname }}</div>
-            <div>{{ $post->sauna_name }}</div>
-            <div>{{ $post->image_path }}</div>
-            <div>{{ $post->content }}</div>
+        <a href="#" class="btn btn-outline-primary me-2 mb-3">投稿</a>
 
-            <a href="#">編集</a>
-            <a href="#">削除</a>
+        @foreach ($posts as $post)
+            <div class="border">
+                <div class="m-4">
+                    <h5>投稿者: {{ $post->nickname }}</h5>
+                    <h5>施設の名前: {{ $post->sauna_name }}</h5>
+                    <div>{{ $post->image_path }}</div>
+                    <p>{{ $post->content }}</p>
+    
+                    <div>
+                        <a href="#" class="btn btn-outline-primary me-2">詳細</a>
+                        <a href="#" class="btn btn-outline-primary me-2">編集</a>
+                        <a href="#" class="btn btn-outline-danger">削除</a>
+                    </div>
+                </div>
+            </div>
+            <br>
         @endforeach
     </div>
 @endsection
