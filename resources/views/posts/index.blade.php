2a9597c77a73d09f1e67badbe537648a70890c7a
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-05 10:10:09 +0900

    削除できるボタンの作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,13 +11,17 @@
                     <h5>施設の名前: {{ $post->sauna_name }}</h5>
                     <p>口コミ: <br>{{ $post->content }}</p>
                     @if ($post->image_path)
-                    <div class="m-2"><img src="upload/{{ $post->image_path }}" width="300"></div>
+                        <div class="m-2"><img src="upload/{{ $post->image_path }}" width="300"></div>
                     @endif
-    
+
                     <div>
-                        <a href="#" class="btn btn-outline-primary me-2">コメントをする</a>
-                        <a href="{{ route('post.edit', $post)}}" class="btn btn-outline-primary me-2">編集</a>
-                        <a href="#" class="btn btn-outline-danger">削除</a>
+                        <a href="#" class="btn btn-outline-primary ms-5">コメントをする</a>
+                        <a href="{{ route('post.edit', $post) }}" class="btn btn-outline-primary ms-3">編集</a>
+                        <form action="{{ route('post.destroy', $post)}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-outline-danger mt-2 ms-5">削除</button>
+                        </form>
                     </div>
                 </div>
             </div>
