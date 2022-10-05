25f148c232b09452269edaa17bcebb424a118cc5
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-05 16:02:41 +0900

    user_idカラムの追加

--- database/migrations/2022_10_04_012738_create_posts_table.php
+++ database/migrations/2022_10_04_012738_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('nickname')->comment('投稿者のニックネーム');
+            $table->integer('user_id');
             $table->string('sauna_name')->comment('サウナ施設の名前');
             $table->string('image_path')->comment('サウナ施設等の画像');
             $table->string('content')->comment('サウナの内容');
