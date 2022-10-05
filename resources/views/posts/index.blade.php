3ea0f8d0f65fa7165f3e8d710c6392590a47d22a
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-05 16:04:49 +0900

    投稿者の名前が表示されるように処理を追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
         @foreach ($posts as $post)
             <div class="border">
                 <div class="m-4">
-                    <h5>投稿者: {{ $post->nickname }}</h5>
+                    <a href="{{route('user.detail', $post->user_id)}}" style="text-decoration: none; color: black"><h5>投稿者: {{ $post->user->name }}</h5></a>
                     <h5>施設の名前: {{ $post->sauna_name }}</h5>
                     <p>口コミ: <br>{{ $post->content }}</p>
                     @if ($post->image_path)
@@ -15,13 +15,13 @@
                     @endif
 
                     <div style="display:inline-flex">
-                        <a href="{{ route('post.show', $post) }}" class="btn btn-outline-primary ms-4">コメントをする</a>
-                        <a href="{{ route('post.edit', $post) }}" class="btn btn-outline-primary ms-3">編集</a>
+                        <a href="{{ route('post.show', $post) }}" class="btn btn-outline-primary ms-2">コメントをする</a>
+                        {{-- <a href="{{ route('post.edit', $post) }}" class="btn btn-outline-primary ms-3">編集</a>
                         <form action="{{ route('post.destroy', $post)}}" method="post">
                             @csrf
                             @method('delete')
                             <button type="submit" class="btn btn-outline-danger ms-3">削除</button>
-                        </form>
+                        </form> --}}
                     </div>
                 </div>
             </div>
