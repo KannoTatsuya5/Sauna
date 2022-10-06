92343c69c40d81145ce8399e6b7a803708e98516
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-06 10:32:01 +0900

    による投稿を取得する記述が不完全であったため、改修

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,10 @@ use App\Models\Post;
 class UserController extends Controller
 {
     public function detail(User $user) {
-        $user = User::find($user->id); //idが、リクエストされた$userのidと一致するuserを取得
-        $posts = Post::where('user_id', $user->id) //$userによる投稿を取得
-            ->orderBy('created_at', 'desc');
 
-        return view('users.user_detail', compact('user'));
+        $user = User::find($user->id); //idが、リクエストされた$userのidと一致するuserを取得
+        $posts = Post::where('user_id', $user->id)->get(); //$userによる投稿を取得
+
+        return view('users.user_detail', compact('user', 'posts'));
     }
 }
