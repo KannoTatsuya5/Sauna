ae2ec450c80fdae6ad34d5da5593b236b4b2eda3
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-06 13:55:11 +0900

    hasMany()メソッドの追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Reply;
 
 class Post extends Model
 {
@@ -12,5 +13,9 @@ class Post extends Model
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function replies() {
+        return $this->hasMany(Reply::class);
     }
 }
