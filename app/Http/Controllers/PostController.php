4720265b77321f654db3beda13db853e6850f401
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-11 16:52:18 +0900

    コメントを細かく書いて、意味が分かりやすくした

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
-use App\Models\User;
 use App\Models\Reply;
-use App\Models\Nice;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -22,36 +20,39 @@ class PostController extends Controller
         // 新しい順で一覧表示(user情報を渡している)
         $posts = Post::with('user')->with('nices')->latest()->paginate(5);
 
+        // 検索する単語を$searchに代入
         $search = $request->input('search');
 
+        // メッセージのデフォルトはnullに設定
         $no_item_message = null;
 
         // クエリビルダ
         $query = Post::query();
 
-        // もし検索フォームにキーワードが入力されたら
+        // 検索したときの処理
         if ($search) {
 
             // 全角スペースを半角に変換
             $spaceConversion = mb_convert_kana($search, 's');
 
-            // 単語を半角スペースで区切り、配列にする（例："山田 翔" → ["山田", "翔"]）
+            // 単語を半角スペースで区切り、配列にする（例："サウナ　カプセル" → ["サウナ", "カプセル"]）
             $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
 
 
-            // 単語をループで回し、ユーザーネームと部分一致するものがあれば、$queryとして保持される
+            // 単語をループで回し、投稿したタイトルと部分一致するものがあれば、$queryとして保持される
             foreach ($wordArraySearched as $value) {
                 $query->where('sauna_name', 'like', '%' . $value . '%')->latest();
             }
             $posts = $query->paginate(5);
 
+            // 検索結果がnullだった場合、
             if ($posts[0] == null) {
                 $no_item_message = "該当する施設の口コミがありません。他のキーワードで検索してみよう!";
             }
         }
 
 
-        //変数$postsをposts/index.blade.phpに渡す
+        // 変数$postsをposts/index.blade.phpに渡す
         return view('posts.index', compact('posts'))->with([
             'post' => $posts,
             'search' => $search,
@@ -77,7 +78,10 @@ class PostController extends Controller
      */
     public function store(Request $request, Post $post)
     {
+        // ファイルを代入
         $file = $request->file('image_path');
+
+        // 画像が入力された際の保存先を指定
         if (!empty($file)) {
             $filename = $file->getClientOriginalName();
             $file->move('./upload/', $filename);
@@ -107,7 +111,9 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
+        // ユーザー情報をpostと結合
         $posts = Post::with('user')->latest()->get();
+        // 投稿とコメントを紐づける
         $replies = Reply::where('post_id', $post->id)->paginate(10);
 
         return view('posts.show', compact('posts', 'post', 'replies'))->with('user_id', Auth::id());
@@ -133,7 +139,10 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
+        // ファイルを代入
         $file = $request->file('image_path');
+
+        // ファイルが入力されたときの保存先の指定
         if (!empty($file)) {
             $filename = $file->getClientOriginalName();
             $file->move('./upload/', $filename);
@@ -161,6 +170,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        // 削除
         $post->delete();
         return redirect()->route('post.index', $post)->with('message', '投稿を削除しました。');
     }
