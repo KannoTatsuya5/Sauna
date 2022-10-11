9c774aa23e7b9f62dafedb5058f62c532e01a15d
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2022-10-11 09:31:20 +0900

    いいね機能の追加の記述

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,8 +7,10 @@
         @foreach ($posts as $post)
             <div class="border">
                 <div class="m-4">
-                    <p>{{$post->updated_at}}</p>
-                    <a href="{{route('user.detail', $post->user_id)}}" style="text-decoration: none; color: black"><h5>投稿者: {{ $post->user->name }}</h5></a>
+                    <p>{{ $post->updated_at }}</p>
+                    <a href="{{ route('user.detail', $post->user_id) }}" style="text-decoration: none; color: black">
+                        <h5>投稿者: {{ $post->user->name }}</h5>
+                    </a>
                     <h5>施設の名前: {{ $post->sauna_name }}</h5>
                     <p>口コミ: <br>{{ $post->content }}</p>
                     @if ($post->image_path)
@@ -17,13 +19,41 @@
 
                     <div style="display:inline-flex">
                         <a href="{{ route('post.show', $post) }}" class="btn btn-outline-primary ms-2">コメントをする</a>
-                        {{-- <a href="{{ route('post.edit', $post) }}" class="btn btn-outline-primary ms-3">編集</a>
-                        <form action="{{ route('post.destroy', $post)}}" method="post">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-outline-danger ms-3">削除</button>
-                        </form> --}}
                     </div>
+                    <span>
+                        <!-- もし$niceがあれば＝ユーザーが「いいね」をしていたら -->
+                        @if (!$post->nices->isEmpty())
+                            @foreach ($post->nices as $nice)
+                                @if ($nice->user_id == Auth::id())
+                                    <!-- 「いいね」取消用ボタンを表示 -->
+                                    <a href="{{ route('unnice', $post) }}" class="btn btn-success ms-2">
+                                        ❤️
+                                        <!-- 「いいね」の数を表示 -->
+                                        <span class="badge">
+                                            {{ $post->nices->count() }}
+                                        </span>
+                                    </a>
+                                @else
+                                    <a href="{{ route('nice', $post) }}" class="btn btn-primary ms-2">
+                                        ❤️
+                                        <!-- 「いいね」の数を表示 -->
+                                        <span class="badge">
+                                            {{ $post->nices->count() }}
+                                        </span>
+                                    </a>
+                                @endif
+                            @endforeach
+                        @else
+                            <!-- まだユーザーが「いいね」をしていなければ、「いいね」ボタンを表示 -->
+                            <a href="{{ route('nice', $post) }}" class="btn btn-primary ms-2">
+                                ❤️
+                                <!-- 「いいね」の数を表示 -->
+                                <span class="badge">
+                                    {{ $post->nices->count() }}
+                                </span>
+                            </a>
+                        @endif
+                    </span>
                 </div>
             </div>
             <br>
